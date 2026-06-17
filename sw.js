16611353c1dd56b9556ddfddca03e50617a1f008
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-17 05:29:23 +0000

    Update service worker to version 2.9

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION = "v2.8";
+const APP_VERSION = "v2.9";
 const SONG_CACHE = "v1.0";
 
 const SONG_CACHE = `alatipha-music-beta-${APP_VERSION}`;
